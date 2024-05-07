09773193b3d74e24b5569ef0c02393dbbaf57d59
victorr0909 <namit09092001@gmail.com>
2024-05-07 14:22:21 +0530

    Functions

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int A, int B){
+    int C;
+    C=A+B;
+    return(C);
+}
+
+int main (){
+    int x= 10,y=5, s;
+    s= add(x, y);
+    cout<<s;
+}
