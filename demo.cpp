cfacc46e0eedc6b81f789d455c5da9add3d2dbaa
victorr0909 <namit09092001@gmail.com>
2024-05-19 14:13:33 +0530

    Strivers

--- demo.cpp
+++ demo.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main(){
-    int x;
-    cin>> x;
-    cout<< "Hey " << x; 
+    // int x;
+    // cin>> x;
+    // cout<< "Hey " << x; 
+    // int , long, long long, double, float
+    // string and getline()
+    // char
+
+
 }
